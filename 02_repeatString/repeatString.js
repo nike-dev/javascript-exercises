287c17aeb574745a7a610b88e7d86e4da2c0729b
nike-dev <146763214+nike-dev@users.noreply.github.com>
2025-02-02 00:18:03 +0200

    Create a function to repeat a string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num ) {
+    let i = 1;
+    let finalString = "";
+    while (i <= num) {
+        finalString += string ;
+        i++;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
