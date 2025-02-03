d949482807d25f41376021680c829c735b55ec00
nike-dev <146763214+nike-dev@users.noreply.github.com>
2025-02-03 12:23:49 +0200

    Exercises 4 and 5 completed with all tests pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function (startInt, endInt) {
+    let sum = 0;
+    if (startInt < 0 || endInt < 0) return sum = "ERROR";
+    else if (Math.floor(startInt) !== startInt
+        || Math.floor(endInt) !== endInt) return sum = "ERROR";
+    else if (typeof startInt !== typeof endInt) return sum = "ERROR";
+    else if (endInt > startInt) {
+        while (startInt <= endInt) {
+            sum += startInt;
+            startInt++;
+        }
+    } else {
+        while (startInt >= endInt) {
+            sum += startInt;
+            startInt--;
+        }
+    }
+
+    return sum;
 
 };
 
