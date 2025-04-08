3dcc3b8f5fd21138bc1ec671d322aee443a6c89c
nike-dev <146763214+nike-dev@users.noreply.github.com>
2025-04-08 23:58:10 +0300

    Solve exercises 08 & 09

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,31 +1,27 @@
-const add = function(num1, num2) {
+const add = function (num1, num2) {
   return num1 + num2;
-	
+
 };
 
-const subtract = function(num1, num2) {
-	return num1 - num2;
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function(array) {
-  let sum = 0;
-  array.forEach(element => {
-    sum += element;
-  });
-  return sum;
+const sum = function (array) {
+  return array.reduce((total, item) => {
+    return total + item
+  }, 0)
 };
 
-const multiply = function(array) {
-  let product = 1;
-  for (num of array) {
-    product *= num;
-  }
-  return product;
-};
+const multiply = function (array) {
+  return array.reduce((product, item) => {
+    return product * item;
+    }, 1
+)}
 
-const power = function(number, raiser) {
+const power = function (number, raiser) {
   let powered = 1;
-	for (let i = 0; i < raiser; i++) {
+  for (let i = 0; i < raiser; i++) {
     powered *= number;
   }
 
@@ -33,11 +29,11 @@ const power = function(number, raiser) {
   // return Math.pow(number, raiser);
 };
 
-const factorial = function(number) {
+const factorial = function (number) {
   let i = 0;
   let factorResult = 1;
   for (i; i < number; i++) {
-	factorResult *= (number - i);
+    factorResult *= (number - i);
   }
 
   return factorResult;
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,14 +1,20 @@
 const palindromes = function (string) {
-    const noPunctuationString = string
-        .replace(/[!,. ]/g, "")
-        .toLowerCase();
-    const stringReversed = noPunctuationString
+    // const noPunctuationString = string
+    //     .replace(/[!,. ]/g, "")
+    //     .toLowerCase();
+    // const stringReversed = noPunctuationString
+    //     .split("")
+    //     .toReversed()
+    //     .join("");
+
+
+    // return noPunctuationString == stringReversed;
+    const noPunctuationString = string.replace(/[!., ]/g, "");
+    const reversedString = noPunctuationString
         .split("")
         .toReversed()
         .join("");
-
-
-    return noPunctuationString == stringReversed;
+    return noPunctuationString.toLowerCase() === reversedString.toLowerCase();
 };
 
 // Do not edit below this line
